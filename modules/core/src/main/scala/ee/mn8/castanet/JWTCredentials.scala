64ae14cfe903f524ca1e7f7c399afe4f42e7ebf9
iandebeer <ian@zenaptix.com>
2021-08-24 15:54:29 +0200

    Transition inline RPC calls

---
+++ modules/core/src/main/scala/ee/mn8/castanet/JWTCredentials.scala
@@ -0,0 +1,32 @@
+package ee.mn8.castanet
+
+/* import io.grpc.CallCredentials
+import java.util.concurrent.Executor
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.MethodDescriptor
+import io.grpc.ClientInterceptor */
+import _root_.io.grpc.*
+import java.util.concurrent.Executor
+
+case class JwtCredentials() extends CallCredentials:
+  import Constants._
+  override def thisUsesUnstableApi(): Unit = {}
+  override def applyRequestMetadata(
+      requestInfo: CallCredentials.RequestInfo,
+      appExecutor: Executor,
+      applier: CallCredentials.MetadataApplier
+  ): Unit =
+    val headers = new Metadata()
+    headers.put[String](AuthorizationMetadataKey, "test")
+    applier.apply(headers)
+
+
+case class KeycloakInterceptor(s: String) extends ClientInterceptor:
+  override def interceptCall[Req, Res](
+      methodDescriptor: MethodDescriptor[Req, Res],
+      callOptions: CallOptions,
+      channel: Channel
+  ) =
+    println("hello from the client")
+    channel.newCall[Req, Res](methodDescriptor, callOptions.withCallCredentials(JwtCredentials()))
---
+++ modules/core/src/main/scala/ee/mn8/castanet/PetriElement.scala
@@ -0,0 +1,56 @@
+package ee.mn8.castanet
+
+import cats.kernel.Monoid
+import scala.collection.immutable.SortedMap
+import scala.collection.immutable.ListSet
+import io.grpc.ManagedChannelBuilder
+
+import cats.effect.*
+import cats.effect.std.Dispatcher
+import fs2.*
+import _root_.io.grpc.*
+import fs2.grpc.syntax.all.*
+import java.util.concurrent.Executor
+import Constants.*
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import fs2.grpc.client.ClientOptions
+import _root_.io.grpc.ClientInterceptor
+
+import cats.effect.IO
+
+trait PetriElement 
+
+trait ConcatenableProcess extends PetriElement with Monoid[PetriElement]
+
+type NodeId = Int
+
+enum Arc extends PetriElement : 
+  val from: NodeId
+  val to: NodeId
+  case Timed(from: NodeId, to: NodeId, interval: Long) extends Arc
+  case Weighted(from: NodeId, to: NodeId, weight: Int) extends Arc
+
+trait  LinkableElement extends PetriElement: 
+  inline def assert[T](condition:Boolean, expr:T)  = 
+    if condition then expr else ()
+  val id: NodeId
+  val name: String
+  def run():Unit
+
+case class Place(id: NodeId, name: String, capacity: Int) extends LinkableElement:
+  def run() = assert(true, println(s"Place: $name"))
+
+case class Transition(id: NodeId, name: String, service:Service, rpc:RPC) extends LinkableElement :
+  
+  val managedChannelStream: Stream[IO, ManagedChannel] =
+    ManagedChannelBuilder
+      .forAddress("127.0.0.1", 9999)
+      .usePlaintext()
+      .intercept(KeycloakInterceptor("hi"))
+      .stream[IO]
+  def run() = assert(true, println(s"Transition: $name"))
+
+case class PetriElements(l: List[LinkableElement] = List[LinkableElement]()) 
+
+type PetriGraph = SortedMap[NodeId, ListSet[LinkableElement]]
